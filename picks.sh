df5dc88b82f16e119cd825b5f2d6de990df720ab
garak <elim.garak.dev@gmail.com>
2023-12-30 17:11:35 +0100

    update files

--- picks.sh
+++ picks.sh
@@ -18,10 +18,7 @@ source build/envsetup.sh
 
 #repopick 320142 320337 321934 318814-318817 320525 320534-320540 320542-320546 331660 331661 335203
 #repopick -t S_asb_2023-02
-repopick -f 320142 320546 321934 318817 326385 331660 331661 335203 && repopick -f -P system/bpf 320591 && repopick -f -P system/netd 320592 && repopick -f -P art 318097 && repopick -f 287706 -P external/perfetto
-cd vendor/samsung/chagall-common/
-git fetch https://github.com/HazouPH/android_vendor_samsung_chagall-common lineage-17.1 && git cherry-pick 00bd8c9f107fa53779fa980843b74d2f58b8c20a
-cd ../../..
+repopick -f 320142 331660 331661 335203
 cd kernel/samsung/exynos5420/
 #git revert --no-edit f9c4050256d3be4a4033a1a035bd67ce20e2267f
 git fetch https://github.com/bluess57/android_kernel_samsung_exynos5420 lineage-17.1 && git cherry-pick 0a33bd51180bc0c62d9c41b64c66aae49f7556be
